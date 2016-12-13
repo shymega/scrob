48fe79e090919e017b36c9e0f46b933954586168
Dom Rodriguez <shymega@shymega.org.uk>
2016-12-13 20:17:40 +0000

    Initial commit

---
+++ src/bin/rscribble.rs
@@ -0,0 +1,43 @@
+// This file is part of Xtensis.
+
+// Xtensis is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Xtensis is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Xtensis.  If not, see <http://www.gnu.org/licenses/>
+
+extern crate clap;
+extern crate rscribble;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn get_arguments() -> ArgMatches<'static> {
+    App::new("rscribble")
+        .version(VERSION)
+        .author("Dom Rodriguez <shymega@shymega.org.uk>")
+        .about("Extensible editing: screw limits.")
+        .about("A modular scrobbler for a variety of music players.")
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .required(false)
+            .help("Sets the level of logging verbosity."))
+        .get_matches()
+}
+
+fn main() {
+    let matches = get_arguments();
+
+    println!("rscribble starting NOW..");
+    println!("Version: {}", VERSION);
+}
