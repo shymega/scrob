4b6048384b56c051b724373ba382e2c8a5af5964
Dom Rodriguez <shymega@shymega.org.uk>
2017-04-18 23:45:57 +0200

    Add docs to functions/types

--- src/sources/mpd/utils.rs
+++ src/sources/mpd/utils.rs
@@ -1,13 +1,16 @@
-use std::collections::BTreeMap;
+//! Utilities module for MPD source.
 
 #[derive(Debug)]
 pub enum RetrieveError {
     InvalidTag,
 }
 
+/// SongTags is a representation of the tags associated with a
+/// song. This is a BTreeMap with two Strings.
 pub type SongTags = BTreeMap<String, String>;
 
-pub fn get_tag(tags: &SongTags, tag: &str) -> Result<String, RetrieveError> {
+/// This function takes tags of SongTags type, and a tag to retrieve.
+pub fn get_tag(tags: &SongTags, tag: &str) -> Result<String, String> {
     match tags.get(tag) {
         Some(x) => Ok(x.to_string()),
         None => Ok("None defined".to_string()),
