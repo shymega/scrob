e6d75da1ad4586bcc443018cdd0370211cd29ecb
Dom Rodriguez <shymega@shymega.org.uk>
2018-11-15 19:08:56 +0000

    autosave tracked files

--- src/sources/mpd/mod.rs
+++ src/sources/mpd/mod.rs
@@ -19,14 +19,14 @@ pub mod utils;
 
 extern crate mpd;
 
-use mpd::Client;
+use self::utils::get_tag;
 use mpd::idle::Idle;
 use mpd::idle::Subsystem::Player;
-use self::utils::get_tag;
+use mpd::Client;
 use std::net::TcpStream;
 use std::process::exit;
 
-use ::sources::common::Song;
+use sources::common::Song;
 
 fn get_mpd_conn(addr: &str) -> Client<TcpStream> {
     match Client::connect(addr) {
@@ -41,6 +41,8 @@ fn get_mpd_conn(addr: &str) -> Client<TcpStream> {
 }
 
 /// Loop over MPD `Player` events, and display the song and artist.
+/// This is for debugging, and only enabled on a debug build.
+#[cfg(debug_assertions)]
 pub fn display_mpd_songs() {
     let addr = "127.0.0.1:6600";
 
@@ -72,11 +74,12 @@ pub fn display_mpd_songs() {
 
             println!("**************************");
             println!("New song playing!");
-            println!("{title}, by {artist}",
-                     title = song.title,
-                     artist = song.artist);
+            println!(
+                "{title}, by {artist}",
+                title = song.title,
+                artist = song.artist
+            );
             println!("**************************");
-
         }
     }
 }
