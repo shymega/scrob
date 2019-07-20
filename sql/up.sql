f046eae77f0241d66c9db8f29c27ed367d5171d8
Dom Rodriguez <shymega@shymega.org.uk>
2019-07-20 20:24:42 +0100

    Update SQL schema

--- sql/up.sql
+++ sql/up.sql
@@ -1,14 +1,29 @@
 -- Initial revision of SQL script.
 
-CREATE TABLE Songs (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    album TEXT NOT NULL,
-    artist TEXT NOT NULL,
-    album_artist TEXT NOT NULL,
-    genre TEXT NOT NULL,
-    track TEXT NOT NULL,
-    composer TEXT NOT NULL,
-    ts_created TEXT NOT NULL,
-    ts_inputted TEXT NOT NULL
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS Songs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  album TEXT NOT NULL,
+  artist TEXT NOT NULL,
+  album_artist TEXT NOT NULL,
+  genre TEXT NOT NULL,
+  track TEXT NOT NULL,
+  composer TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Artists (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Scrobbles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT
+  -- artist
+  -- timestamp of scrobble
+  -- scrobble source
+  -- target(s) <-- enum?
+);
+
+COMMIT TRANSACTION;
