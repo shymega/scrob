aeb0fd1a141bc59ddbc64f25a5e50d958f4ba027
Dom Rodriguez <shymega@shymega.org.uk>
2022-05-18 20:28:58 +0100

    Wrap DB connection initialiser in `Option<T>`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,12 +2,14 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
+pub fn establish_connection() -> Option<SqliteConnection> {
     dotenv().ok();
 
     let database_url =
         env::var("DATABASE_URL").expect("$DATABASE_URL must be set!");
 
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    Some(
+        SqliteConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url)),
+    )
 }
