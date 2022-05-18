a07eb23ce934fa48a3ed87c2cb91aa818dcfb4b7
Dom Rodriguez <shymega@shymega.org.uk>
2022-05-18 20:37:05 +0100

    Revert "Wrap DB connection initialiser in `Option<T>`"
    This reverts commit aeb0fd1a141bc59ddbc64f25a5e50d958f4ba027.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,14 +2,12 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> Option<SqliteConnection> {
+pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url =
         env::var("DATABASE_URL").expect("$DATABASE_URL must be set!");
 
-    Some(
-        SqliteConnection::establish(&database_url)
-            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url)),
-    )
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
