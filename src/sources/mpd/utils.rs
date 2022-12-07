e803eceecdcffbc54aa3824d85d58ec3d7ac251c
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-07 20:18:08 +0000

    Change `SongTags` utils to `pub(crate)`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/sources/mpd/utils.rs
+++ src/sources/mpd/utils.rs
@@ -4,10 +4,10 @@ use std::collections::BTreeMap;
 
 /// SongTags is a representation of the tags associated with a
 /// song. This is a BTreeMap with two Strings.
-pub type SongTags = BTreeMap<String, String>;
+pub(crate) type SongTags = BTreeMap<String, String>;
 
 /// This function takes tags of SongTags type, and a tag to retrieve.
-pub fn get_tag(tags: &SongTags, tag: &str) -> Option<String> {
+pub(crate) fn get_tag(tags: &SongTags, tag: &str) -> Option<String> {
     match tags.get(tag) {
         Some(t) => Some(t),
         None => None,
