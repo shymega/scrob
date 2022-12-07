f464d6445b852e99b72f1d1ee318bb45d11393a1
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-07 19:53:13 +0000

    db: Introduce idiomatic `Error` handling, and better UX
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,14 +1,70 @@
 use diesel::prelude::*;
 use dotenv::dotenv;
-use std::env;
+use std::env::{self, VarError};
+use std::error;
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug)]
+pub(crate) enum DbInitialisationError {
+    SqliteConnectionError(ConnectionError),
+    EnvVarError(VarError),
+    DotEnvGetError,
+}
+
+impl error::Error for DbInitialisationError {}
+
+impl fmt::Display for DbInitialisationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &*self {
+            DbInitialisationError::DotEnvGetError => {
+                write!(f, "Error with `dotenv`. Missing `.env`?")
+            }
+            DbInitialisationError::EnvVarError(e) => {
+                write!(
+                    f,
+                    "Error getting env variables - error: {}",
+                    e.to_string()
+                )
+            }
+            DbInitialisationError::SqliteConnectionError(e) => {
+                write!(
+                    f,
+                    "Error connecting to SQLite DB - error: {}",
+                    e.to_string()
+                )
+            }
+        }
+    }
+}
+
+impl From<ConnectionError> for DbInitialisationError {
+    fn from(e: ConnectionError) -> Self {
+        Self::SqliteConnectionError(e)
+    }
+}
+
+impl From<VarError> for DbInitialisationError {
+    fn from(e: VarError) -> Self {
+        Self::EnvVarError(e)
+    }
+}
 
 #[allow(dead_code)]
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
+pub(crate) fn establish_connection(
+) -> Result<SqliteConnection, DbInitialisationError> {
+    match dotenv().ok() {
+        Some(_) => (),
+        None => return Err(DbInitialisationError::DotEnvGetError),
+    };
 
-    let database_url =
-        env::var("DATABASE_URL").expect("$DATABASE_URL must be set!");
+    let db_url = match env::var("DATABASE_URL") {
+        Ok(url) => url,
+        Err(e) => return Err(DbInitialisationError::EnvVarError(e)),
+    };
 
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    match SqliteConnection::establish(&db_url) {
+        Ok(conn) => Ok(conn),
+        Err(e) => Err(DbInitialisationError::SqliteConnectionError(e)),
+    }
 }
