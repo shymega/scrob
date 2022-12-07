d1ddd672487c5aa6434fadf7533492d6e62d2eca
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-07 20:17:58 +0000

    Use Option<T> for `Song` struct
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/sources/common.rs
+++ src/sources/common.rs
@@ -1,12 +1,12 @@
 /// Struct for a song.
-#[derive(Debug, Default, Clone)]
-pub struct Song {
-    pub title: String,
-    pub artist: String,
-    pub album: String,
-    pub album_artist: String,
-    pub date: String,
-    pub genre: String,
-    pub track: String,
-    pub composer: String,
+#[derive(Debug, Default)]
+pub(crate) struct Song {
+    title: Option<String>,
+    artist: Option<String>,
+    album: Option<String>,
+    album_artist: Option<String>,
+    date: Option<String>,
+    genre: Option<String>,
+    track: Option<String>,
+    composer: Option<String>,
 }
