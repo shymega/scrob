63749e9e4a8b73017ffa3156b9176a4148dabc0c
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-07 19:47:46 +0000

    bin(scrobctl): Fix broken Clap arg handler
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/scrobctl.rs
+++ src/bin/scrobctl.rs
@@ -16,23 +16,22 @@
     variant_size_differences
 )]
 
-use clap::{Arg, ArgMatches, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
-fn get_arguments() -> Option<ArgMatches> {
-    Some(
-        Command::new("scrobc")
-            .version(env!("CARGO_PKG_VERSION"))
-            .author("Dom Rodriguez <shymega@shymega.org.uk>")
-            .about("Client for controlling scrob")
-            .arg(
-                Arg::new("v")
-                    .short('v')
-                    .multiple_occurrences(true)
-                    .required(false)
-                    .help("Sets the level of logging verbosity."),
-            )
-            .get_matches(),
-    )
+fn get_arguments() -> ArgMatches {
+    Command::new("scrobc")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Dom Rodriguez <shymega@shymega.org.uk>")
+        .about("Client for controlling `scrobd`")
+        .subcommand_required(true)
+        .arg(
+            Arg::new("verbosity")
+                .short('v')
+                .action(ArgAction::Count)
+                .required(false)
+                .help("Sets the level of logging verbosity."),
+        )
+        .get_matches()
 }
 
 fn main() {
