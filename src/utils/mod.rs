dedbba5ea82a1aca16ec8bc2a52c4ec8768e8349
Dom Rodriguez <shymega@shymega.org.uk>
2023-03-30 17:42:12 +0100

    Add derived StrType custom type
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,11 @@
 //! Module exporting various utilities for `scrob`, including logging.
 
+use std::borrow::Borrow;
+
 mod logging;
 
 pub use logging::*;
+
+/// `StrType` is derived from a PR on `listenbrainz-rs`.
+pub trait StrType: Borrow<str> {}
+impl<T: Borrow<str>> StrType for T {}
