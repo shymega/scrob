6165824742f79851d775883fb870b4b3a584891c
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-20 23:52:28 +0100

    db: Simplify error handling

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,66 +5,28 @@ use std::error;
 use std::fmt;
 use std::fmt::Formatter;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub(crate) enum DbInitialisationError {
-    SqliteConnectionError(ConnectionError),
-    EnvVarError(VarError),
-    DotEnvGetError,
+    #[error("Error connecting to SQLite database.")]
+    SqliteConnectionError(#[source] ConnectionError),
+    #[error("Error getting env vars.")]
+    EnvVarError(#[source] VarError),
+    #[error("Error with `.env`. Is it created?")]
+    DotEnvGetError(#[source] dotenv::Error)
 }
 
-impl error::Error for DbInitialisationError {}
-
-impl fmt::Display for DbInitialisationError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match &*self {
-            DbInitialisationError::DotEnvGetError => {
-                write!(f, "Error with `dotenv`. Missing `.env`?")
-            }
-            DbInitialisationError::EnvVarError(e) => {
-                write!(
-                    f,
-                    "Error getting env variables - error: {}",
-                    e.to_string()
-                )
-            }
-            DbInitialisationError::SqliteConnectionError(e) => {
-                write!(
-                    f,
-                    "Error connecting to SQLite DB - error: {}",
-                    e.to_string()
-                )
-            }
-        }
-    }
-}
-
-impl From<ConnectionError> for DbInitialisationError {
-    fn from(e: ConnectionError) -> Self {
-        Self::SqliteConnectionError(e)
-    }
-}
-
-impl From<VarError> for DbInitialisationError {
-    fn from(e: VarError) -> Self {
-        Self::EnvVarError(e)
-    }
-}
-
-#[allow(dead_code)]
 pub(crate) fn establish_connection(
 ) -> Result<SqliteConnection, DbInitialisationError> {
-    match dotenv().ok() {
-        Some(_) => (),
-        None => return Err(DbInitialisationError::DotEnvGetError),
-    };
+    dotenv()
+        .ok()
+        .ok_or(DbInitialisationError::DotEnvGetError)?;
 
-    let db_url = match env::var("DATABASE_URL") {
-        Ok(url) => url,
-        Err(e) => return Err(DbInitialisationError::EnvVarError(e)),
-    };
+    let url = env::var("DATABASE_URL")
+        .ok()
+        .ok_or(DbInitialisationError::EnvVarError)?;
 
-    match SqliteConnection::establish(&db_url) {
-        Ok(conn) => Ok(conn),
-        Err(e) => Err(DbInitialisationError::SqliteConnectionError(e)),
-    }
+
+    SqliteConnection::establish(&url)
+        .ok()
+        .ok_or(DbInitialisationError::SqliteConnectionError)?;
 }
