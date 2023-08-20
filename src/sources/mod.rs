3751410bf78c5764864a9f209aa7c1cc478ee9ff
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-20 23:49:54 +0100

    Remove sources code, aim to replace with WASI

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,9 +1,3 @@
 //! This module defines the MPD and MPRIS sources.
 
 pub(crate) mod common;
-
-#[cfg(feature = "mpris-source")]
-pub(crate) mod mpris;
-
-#[cfg(feature = "mpd-source")]
-pub(crate) mod mpd;
