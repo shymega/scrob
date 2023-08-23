b6fdbdd333cdbccd0850fd3fc00b3f7fd37b4f1e
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-23 15:57:08 +0100

    fix(db): Fix broken Error handling in DB module

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,6 @@
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env::{self, VarError};
-use std::error;
-use std::fmt;
-use std::fmt::Formatter;
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum DbInitialisationError {
@@ -12,21 +9,17 @@ pub(crate) enum DbInitialisationError {
     #[error("Error getting env vars.")]
     EnvVarError(#[source] VarError),
     #[error("Error with `.env`. Is it created?")]
-    DotEnvGetError(#[source] dotenv::Error)
+    DotEnvGetError,
 }
 
 pub(crate) fn establish_connection(
 ) -> Result<SqliteConnection, DbInitialisationError> {
-    dotenv()
-        .ok()
+    dotenv().ok()
         .ok_or(DbInitialisationError::DotEnvGetError)?;
 
     let url = env::var("DATABASE_URL")
-        .ok()
-        .ok_or(DbInitialisationError::EnvVarError)?;
+        .map_err(DbInitialisationError::EnvVarError)?;
 
-
-    SqliteConnection::establish(&url)
-        .ok()
-        .ok_or(DbInitialisationError::SqliteConnectionError)?;
+    Ok(SqliteConnection::establish(&url)
+        .map_err(DbInitialisationError::SqliteConnectionError)?)
 }
