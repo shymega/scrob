beeeab245a14fab9ca75fdb828acdf87da8c7332
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-21 15:27:26 +0000

    refactor(major): Initial (temporary) commit of a refactor
    This is a temporary commit which will eventually be split into multiple
    commits. Expect force-pushes. The idea is to refactor `scrob` into a new
    codebase.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,69 +1,37 @@
-# SPDX-FileCopyrightText: 2023 Dom Rodriguez
-#
-# SPDX-License-Identifier: MIT
-
 {
+  description = "Nix Flake for `scrob`.";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-
-    nixpkgs-mozilla = {
-      url = "github:mozilla/nixpkgs-mozilla";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.outputs.legacyPackages.${system};
+        in
+        {
+          packages.scrob = pkgs.callPackage ./scrob.nix { };
+          packages.default = self.outputs.packages.${system}.scrob;
 
-  outputs = { self, flake-utils, naersk, nixpkgs, nixpkgs-mozilla }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-
-          overlays = [
-            (import nixpkgs-mozilla)
-          ];
-        };
-
-        toolchain = (pkgs.rustChannelOf {
-          date = "2023-08-03";
-          channel = "stable";
-          sha256 = "sha256-R0F0Risbr74xg9mEYydyebx/z0Wu6HI0/KWwrV30vZo=";
-        }).rust.override {
-          targets = [
-            "x86_64-unknown-linux-musl"
-            "armv7-unknown-linux-musleabi"
-            "armv7-unknown-linux-musleabihf"
-            "aarch64-unknown-linux-musl"
-          ];
-          extensions = [
-            "rust-src"
-            "rustfmt-preview"
-            "llvm-tools-preview"
-          ];
-        };
-
-        naersk' = pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-        };
-
-      in
-      rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [ systemd.dev protobuf ];
-          cargoBuildOptions = (opts: opts ++ [ "--all-features" "--all-targets" ]);
-          release = true;
-          singleStep = true;
-        };
-
-        # For `nix develop` (optional, can be skipped):
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [ toolchain ] ++ (with pkgs; [ pkg-config ]);
-          buildInputs = with pkgs; [ systemd.dev protobuf ];
-        };
-      }
-    );
+          devShells.default = self.packages.${system}.default.overrideAttrs (super: {
+            nativeBuildInputs = with pkgs;
+              super.nativeBuildInputs
+              ++ [
+                cargo-edit
+                clippy
+                rustfmt
+              ];
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          });
+        })
+    // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${final.system}) scrob;
+      };
+    };
 }
